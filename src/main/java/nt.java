dd6e7dc93904e33f8114726de35bd950c099c29c
Sebastian <412957363@qq.com>
2023-01-31 16:04:54 +0800

    master commit2

--- src/main/java/nt.java
+++ src/main/java/nt.java
@@ -2,6 +2,6 @@ public class nt {
     public static void main(String[] args) {
         System.out.println("nt");
         System.out.println("nt2");
-        System.out.println("nt3");
+        System.out.println("master");
     }
 }
